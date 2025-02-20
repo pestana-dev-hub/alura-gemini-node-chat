23a9b44058d5dfe3e3316e4040221ee32929327f
igorspestana <igorspestana@gmail.com>
2025-02-20 07:28:38 -0300

    Adjustment in history

--- chat.js
+++ chat.js
@@ -3,25 +3,25 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const geminiApiKey = process.env.GEMINI_API_KEY
 
-export async function executaChat(mensagem) {
-  const genAI = new GoogleGenerativeAI(geminiApiKey);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos." }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Olá! Obrigado por entrar em contato com o Jornada Viagens. Antes de responder suas dúvidas, pode me informar seu nome?" }],
-      },
-    ],
-    generationConfig: {
-      maxOutputTokens: 1000,
-    }
-  });
+const genAI = new GoogleGenerativeAI(geminiApiKey);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens. Você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos." }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Olá! Obrigado por entrar em contato com o Jornada Viagens. Antes de responder suas dúvidas, pode me informar seu nome?" }],
+    },
+  ],
+  generationConfig: {
+    maxOutputTokens: 1000,
+  }
+});
 
+export async function executaChat(mensagem) {
   const result = await chat.sendMessage(mensagem);
   const response = await result.response;
   return response.text();
