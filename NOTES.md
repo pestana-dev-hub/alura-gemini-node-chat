6df7f0fbea231583ad581dea36a194f3c3909c24
igorspestana <igorspestana@gmail.com>
2025-02-24 15:19:38 -0300

    Add Embeddings

--- NOTES.md
+++ NOTES.md
@@ -11,3 +11,8 @@ Function calling, no contexto da API Gemini, refere-se à capacidade de definir 
 Documentação: https://ai.google.dev/gemini-api/docs/function-calling?hl=pt-br
 
 Exemplos de códigos com a utilização dos recursos do Gemini: https://github.com/google-gemini/cookbook
+
+## 3. Embeddings
+
+Técnica de processamento de linguagem natural que converte texto em um modelo vetorial. Esse modelo vetorial define a localização de cada palavra em um espaço e palavras com significados semelhantes ficam próximas umas das outras. Quando o usuário faz uma pergunta é feito uma busca nesse modelo vetorial para identificar similaridades entre palavras que constam na pergunta e o texto de referência que será consultado para criar a resposta adequada.
+Fluxo: Usuário faz uma pergunta > Uma função retorna o arquivo que tem o conteúdo com significado mais próximo da pergunta feita. > É criado um prompt que contém a pergunta do usuário mais o conteúdo de referência para orientar a geração da resposta.
--- chat.js
+++ chat.js
@@ -1,8 +1,20 @@
 import { chat, funcoes } from './inicializaChat.js';
+import { incorporarDocumentos, incorporarPergunta } from './embedding.js';
+
+// Transforma o texto em um modelo vetorial
+const documentos = await incorporarDocumentos(
+    ["A política de cancelamento é de 30 dias antes da viagem, caso contrário, não faremos o reembolso",
+    "Viagem para a Disney, 6 dias, R$ 20.000,00 - Viagem para a Disney, 10 dias, R$ 25.000,00"
+    ]
+);
 
 export async function executaChat(mensagem) {
   console.log("Tamanho do histórico: " + (await chat.getHistory()).length);
-  const result = await chat.sendMessage(mensagem);
+
+  // Antes de enviar a mensagem para a api é feito uma consulta nos arquivos de referência em busca de similaridades entre a mensgaem do usuário e os arquivos. Isso é feito para orientar a resposta gerada pela ia.
+  let doc = await incorporarPergunta(mensagem, documentos);
+  let prompt = mensagem + "talvez esse trecho te ajude a formular a resposta" + doc.text
+  const result = await chat.sendMessage(prompt);
   const response = await result.response;
 
   const content = response.candidates[0].content;
